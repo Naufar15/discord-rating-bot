78c2b7437251ce7f1f39ee2e8e490a5a9308fce1
Naufar15 <dixlano15@gmail.com>
2025-11-01 20:30:27 +0700

    Added /rules command with embed support lagi

--- index.js
+++ index.js
@@ -201,7 +201,7 @@ client.on("interactionCreate", async (interaction) => {
     // === /rules ===
     if (interaction.commandName === "rules") {
       const rulesEmbed = new EmbedBuilder()
-        .setColor("#FFA6A6FF")
+        .setColor("#FFA6A6")
         .setTitle("<:enchanted_book1:1434157118939725834>  Rules & Guidelines")
         .setDescription(
           `Selamat datang di **Barbar Boys**!  

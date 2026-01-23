69aaad397f695d389f89bdcc2c05e9026e0569fc
Naufar15 <dixlano15@gmail.com>
2026-01-23 20:24:32 +0700

    fix: init client.commands collection

--- src/commands/rating.js
+++ src/commands/rating.js
@@ -1,25 +1,8 @@
-const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+async execute(interaction) {
+  const stars = interaction.options.getInteger("stars");
+  const comment = interaction.options.getString("comment");
 
-module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("rating")
-    .setDescription("Give a rating and comment!")
-    .addIntegerOption((option) =>
-      option
-        .setName("stars")
-        .setDescription("Select number of stars (1–5)")
-        .setRequired(true)
-        .setMinValue(1)
-        .setMaxValue(5),
-    )
-    .addStringOption((option) =>
-      option
-        .setName("comment")
-        .setDescription("Write your feedback or review")
-        .setRequired(true),
-    ),
-
-  async execute(interaction) {
-    // logic rating kamu
-  },
-};
+  await interaction.reply(
+    `⭐ **Rating:** ${stars}/5\n💬 **Comment:** ${comment}`
+  );
+}
--- src/commands/rules.js
+++ src/commands/rules.js
@@ -6,6 +6,16 @@ module.exports = {
     .setDescription("Send the server rules embed"),
 
   async execute(interaction) {
-    // logic rules embed
+    const embed = new EmbedBuilder()
+      .setTitle("📜 Server Rules")
+      .setDescription(
+        "1. Be respectful\n" +
+          "2. No spam\n" +
+          "3. No NSFW content\n" +
+          "4. Follow Discord TOS",
+      )
+      .setColor("Blue");
+
+    await interaction.reply({ embeds: [embed] });
   },
 };

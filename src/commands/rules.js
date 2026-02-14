03dbdffa649f00655ca6e6c63d01c666be762241
Naufar15 <dixlano15@gmail.com>
2026-02-14 20:51:09 +0700

    UPDATE RULES

--- src/commands/rules.js
+++ src/commands/rules.js
@@ -16,18 +16,18 @@ Tujuan kami sederhana: menciptakan ruang yang nyaman, seru, dan terbuka untuk se
 Sebelum berinteraksi, **harap baca aturan** berikut agar suasana tetap positif dan menyenangkan.
 ---
 ## ⚙️ **Dashboard**
-- <:Paperwork:1434157121288671262>  Baca semua aturan di <#1153261445736316998>.  
-- 💠 Ambil role kamu di <#1153261498064453722>.  
+- <:Paperwork:1434157121288671262>  BACA SEMUA PERATURAN <#1153261445736316998>.  
+- 💠 AMBIL ROLE  <#1153261498064453722>.  
 ---
 ## 📢 **Announcements**
 - 📣 Info penting dari admin akan muncul di https://discord.com/channels/392301202206883850/1153262880947458078.
 - 📣 Info mod gratisan https://discord.com/channels/392301202206883850/1454182321585656034.  
 - 🔴 sedang stream <#1153262934240284673>.  
-- 🎥 Share link livestream kalian di <#1153302328733077525>.  
+- 🎥 Share link livestream kalian <#1153302328733077525>.  
 ---
 ## 💬 **Community**
-- 💭 Ngobrol bebas di <#1153272087226613770>.  
-- 😂 Post meme kalian di <#1153273809936318535>.  
+- 💭 Ngobrol bebas <#1153272087226613770>.  
+- 😂 Post meme kalian <#1153273809936318535>.  
 - <:Paperwork:1434157121288671262> Upload foto/video random di <#1153272677033844778>.  
 - 📸 Pamer hasil foto kalian di <#1153272916893515847>.  
 - 🎬 Share klip atau highlight di <#1153273148196790323>.  
@@ -37,18 +37,18 @@ Sebelum berinteraksi, **harap baca aturan** berikut agar suasana tetap positif d
 ---
 ## 🎨 **Mod Graphic Fivem**
 - 💬 Diskusi & obrolan seputar fivem di <#1359495147032940677>.  
-- 🖼️ Pamer foto kalian di <#1426079591189057546>.  
+- 🖼️ Pamer foto kalian <#1426079591189057546>.  
 - 💎 Booster Perks Preview <#1419968532870860880>.  
 - 🎬 Preset berbayar di <#1403711998939758612>.  
 - 🎬 Graphic pack berbayar di <#1420685191655067762>.  
 - 🚧 Roads berbayar di <#1426798376007110690>.  
 
-- 🆓 Graphic pack gratis di <#1414937176302555229>.  
-- 🚧 Roads gratis di <#1359495340369252353>.  
-- 🌳 Cari mod vegetasi di <#1416133460426358885>.  
-- ⛺ Bagikan preset ReShade gratis di <#1359499034548506938>.  
-- 🌌 Bahas atau share mod citizen di <#1427370970116784299>.  
-- 🎲 Mod random di <#1371053578994843739>.  
+- 🆓 Graphic pack gratis <#1414937176302555229>.  
+- 🚧 Roads gratis <#1359495340369252353>.  
+- 🌳 Mod Vegetasi <#1416133460426358885>.  
+- ⛺ ReShade Preset gratis <#1359499034548506938>.  
+- 🌌 Citizen <#1427370970116784299>.  
+- 🎲 Random mod <#1371053578994843739>.  
 ---
 ## 📩 **Tickets**
 - 📩 Beli di <#1353952676790013973>.  

bf2e9815cd3b5f6adb1684fc0f40bd9e7f74b43b
Naufar15 <dixlano15@gmail.com>
2026-07-26 18:24:28 +0700

    Update custom status bot lagi

--- src/events/ready.js
+++ src/events/ready.js
@@ -17,7 +17,7 @@ module.exports = {
         {
           name: "Custom Status",
           type: ActivityType.Custom, // Tipe status: Custom
-          state: "BUKA KALO NGGA KETIDURAN", // Teks yang akan muncul di profil bot
+          state: "FAST RESPON KALO GA KETIDURAN", // Teks yang akan muncul di profil bot
         },
       ],
       status: "idle", // Pilihan: 'online', 'idle', 'dnd', 'invisible'
